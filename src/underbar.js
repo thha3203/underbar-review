119ff545f6e9eb75032fbb8b5f0ebb42e50ba2eb
Ryan Olea <ryanaolea@gmail.com>
2021-11-09 17:38:06 -0800

    implemented each/filter/reject/unique , passed almost all tests

--- src/underbar.js
+++ src/underbar.js
@@ -50,6 +50,15 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if (Array.isArray(collection)) {
+      for (let i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    } else {
+      for (let key in collection) {
+        iterator(collection[key], key, collection);
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -70,17 +79,71 @@
   };
 
   // Return all elements of an array that pass a truth test.
-  _.filter = function(collection, test) {
+  _.filter = function(collection, test, inverse) {
+    // result is an array
+    let result = [];
+    // iterate through collection
+    _.each(collection, function(element) {
+      if (inverse) {
+        if (!test(element)) {
+          result.push(element);
+        }
+      } else {
+        if (test(element)) {
+          result.push(element);
+        }
+      }
+
+    });
+    return result;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    let result = [];
+    // _.each(collection, function(element) {
+    //   if (test(element) === false) {
+    //     result.push(element);
+    //   }
+    // // });
+    // result = _.filter(collection, test, true);
+    // console.log('this is the result:', result);
+
+
+    result = _.filter(collection, function(element) {
+      return !test(element);
+    });
+
+    return result;
+
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array, isSorted, iterator) {
+    var result = [];
+
+    if (iterator) {
+      for (let i = 0; i < array.length; i++) {
+        array[i] = iterator(array[i]);
+      }
+    }
+
+    var objCount = {};
+    for (let i = 0; i < array.length; i++) {
+      if (objCount[array[i]] === undefined) {
+        result.push(array[i]);
+        objCount[array[i]] = 1;
+      }
+    }
+
+    if (isSorted) {
+      result.sort();
+    }
+
+    return result;
+
   };
 
 
